a697831fc2a5699e1a704c97cc5c4b220a48caf3
Daniil <daniil.pron4enkov@gmail.com>
2021-05-15 20:21:41 +0300

    Create ace editor, button format, styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm'; 
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
+import AceEditor from 'react-ace';
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-terminal";
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import './code-editor.css';
 
 const App = () => {
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
     const ref = useRef<any>();
+    const aceRef = useRef<any>();
+    const iframe = useRef<any>();
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -25,6 +33,8 @@ const App = () => {
             return;
         }
 
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -39,19 +49,76 @@ const App = () => {
             }
         });
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     }
 
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener('message', (event) => {
+                        try {
+                            eval(event.data);
+                        } catch(err) {
+                            const root = document.getElementById('root');
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                            console.error(err);
+                        }
+                    }, false);
+                </script>
+            </body>
+        </html>
+    `;
+
+    const onFormatClick = () => {
+        const unformatted = aceRef.current.editor.getValue();
+        const formatted = prettier.format(unformatted, {
+            parser: 'babel',
+            plugins: [parser],
+            useTabs: false,
+            semi: true,
+            singleQuote: true
+        }).replace(/\n$/, '');
+        
+        aceRef.current.editor.setValue(formatted);
+    };
+
     return (
-        <div>
-            <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <div className="editor-wrapper">
+            <button className="button button-format is-primary is-small" onClick={onFormatClick}>Format</button>
+            <AceEditor
+                ref={aceRef}
+                onChange={(value) => setInput(value)}
+                value={input}
+                theme="terminal" 
+                mode="javascript" 
+                height="500px"
+                width="100%"
+                setOptions={{
+                    wrap: true,
+                    fontSize: 16,
+                    indentedSoftWrap: false,
+                    highlightActiveLine: false,
+                    showPrintMargin: false
+                }}
+            />
+            <textarea 
+                value={input} 
+                onChange={(e) => {
+                    setInput(e.target.value)
+                }}
+            ></textarea>
             <div>
                 <button onClick={onClick}>Sumbit</button>
             </div>
-            <pre>{code}</pre>
+            <iframe ref={iframe} title="ide" sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 }
+
 
 ReactDOM.render(
     <App />,
